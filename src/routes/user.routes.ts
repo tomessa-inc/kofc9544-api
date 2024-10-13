488b8c19f12591bf2d7bbf2a7522f3ff85a470e2
Tom <tomc@tomvisions.com>
2024-10-13 13:05:22 -0400

    lateat files

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,8 +3,10 @@ import expressRouter from 'express';
 const userRouter = expressRouter.Router();
 import {UserController} from "../controllers/user.controller";
 //import {UserAvatarController} from "../controllers/user.avatar.controller";
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
-userRouter.post("/sign-in", UserController.apiPostSignIn);
+userRouter.post("/sign-in", jsonParser, UserController.apiPostSignIn);
 userRouter.post("/sign-out", UserController.apiPostSignOut);
 userRouter.post("/sign-up", UserController.apiPostSignUp);
 userRouter.get("/", UserController.getAllUsers);
