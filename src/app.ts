dfbc32b0f696316de884d808b26a35c6836041b4
Tom <tomc@tomvisions.com>
2024-10-13 12:15:41 -0400

    lateat files

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ import bodyparser from 'body-parser';
 app.use(express.static("/tmp"));
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json({limit: '50mb'}));
+app.use(express.json())
 app.use(cors());
 app.use(compression());
 
