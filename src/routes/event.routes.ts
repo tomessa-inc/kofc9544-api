195c452124a5c631d1f1c4428d5e475816bd316c
Tom <tomc@tomvisions.com>
2024-10-19 21:53:21 -0400

    lateat files

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -4,7 +4,7 @@ const eventRouter = expressRouter.Router();
 import {EventController} from "../controllers/event.controller";
 
 eventRouter.post("/", EventController.apiCreateEvent);
-eventRouter.post("/month", EventController.apiGetEventsMonthByDay);
+eventRouter.get("/month", EventController.apiGetEventsMonthByDay);
 eventRouter.post("/publish", EventController.apiPublishEvents);
 
 export {eventRouter }
