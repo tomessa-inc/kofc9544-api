aa32e4797b4f94a880da70d9370bb53c8f4e7112
Tom Cruickshank <tomc@tomvisions.com>
2026-04-07 18:23:57 -0400

    fixed

--- src/boot2.ts
+++ src/boot2.ts
@@ -1,27 +1,18 @@
-import {MailMapper} from "./mapper/mail.mapper";
-import {MailControllerBak} from "./controllers/mail.controller.ts.bak";
+import { MailMapper } from "./mapper/mail.mapper";
+import { mailMapper } from "./mapper";
 
-async function Run() {
-    const test = {
-    "email": "tcricksh@gmail.com",
-    "name" : "tom",
-    "body": "Would like to discuss something that is really cool. Woot!",
-    "email_type": "contact-us",
-    "subject": "Requesting time to check things blah blah",
-    "phone": "613-111-1111"
+async function run() {
+    const data = {
+        email:      "tcricksh@gmail.com",
+        name:       "tom",
+        body:       "Would like to discuss something that is really cool. Woot!",
+        email_type: "contact-us",
+        subject:    "Requesting time to check things blah blah",
+        phone:      "613-111-1111",
     };
-    const body = {}
-    body['body'] = test;
 
-   // const mail = await new MailController();
-    const retval = await MailControllerBak.apiPostSendMail(body, null, null);
-    console.log(retval);
-//                params = `{"${PARAMS_ID}":"${req.body[PARAMS_ID]}","${PARAMS_GAME}":"${req.body[PARAMS_GAME]}"}`;
-// const gameObject = await MailMapper.apiSendMail(id, game);
-//          console.log('the game');
-//  console.log(gameObject)
-//            return res.status(200).json({result: "success", message: `an issue here ${util.inspect(test)}`});
-   // await MailController.apiSendMail()
+    const result = await mailMapper.setupEmail({ email_type: data.email_type, data });
+    console.log(result);
 }
 
-Run();
+run();
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,6 @@
 import "source-map-support/register";
 import serverlessExpress from "@vendia/serverless-express";
+import { toNodeListener } from "h3";
 import { app } from "./app";
 
-export const handler = serverlessExpress({ app });
+export const handler = serverlessExpress({ app: toNodeListener(app) });
