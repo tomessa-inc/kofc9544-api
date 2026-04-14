fc0f82cc318bbb5e2449d6dba7a95c92b1490c39
Tom Cruickshank <tomc@tomvisions.com>
2026-04-14 19:30:18 -0400

    fixed

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,6 @@
 import "source-map-support/register";
-import serverlessExpress from "@vendia/serverless-express";
+import serverlessHttp from "serverless-http";
 import { toNodeListener } from "h3";
 import { app } from "./app";
 
-export const handler = serverlessExpress({ app: toNodeListener(app) });
+export const handler = serverlessHttp(toNodeListener(app));
