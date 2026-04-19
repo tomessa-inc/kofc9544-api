7da1f7899de3b33e73cb2dd04a4dcc69a198c2f1
Tom Cruickshank <tomc@tomvisions.com>
2026-04-19 09:36:55 -0400

    fixed

--- src/routes/media.routes.ts
+++ src/routes/media.routes.ts
@@ -13,7 +13,7 @@ mediaRouter.post("/publish",                                                    
 
 // Image
 mediaRouter.get("/image/id/:imagesByGalleryId/:pageIndex/:pageSize/:sort/:order",                  defineEventHandler(ImageController.apiGetAllImagesByGallery));
-mediaRouter.post("/image/id/:id/reorder",                                                      defineEventHandler(ImageController.apiImagesOrder));
+//mediaRouter.post("/image/id/:id/reorder",                                                      defineEventHandler(ImageController.apiImagesOrder));
 mediaRouter.post("/id/:galleryId/update/:id/order/:order",                              defineEventHandler(ImageController.apiUpdateOrderImage));
 mediaRouter.get("/primary/logged/:logged",                                               defineEventHandler(ImageController.apiGetAllPrimaryImages));
 mediaRouter.get("/tag/list/frontend",                                                    defineEventHandler(ImageController.apiGetAllPrimaryImages));
