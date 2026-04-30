ac561a9fa1ea28cc873de2ce89432d640040623c
Tom Cruickshank <tomc@tomvisions.com>
2026-04-29 22:36:48 -0400

    fixed bug

--- src/controllers/golf.controller.ts
+++ src/controllers/golf.controller.ts
@@ -322,7 +322,7 @@ export class GolfController {
                 individual: false,
                 email_type:"register"
             }
-            console.log("defiend")
+            console.log("defe")
             console.log(bodyArray)
 //            bodyArray.push(body);
 /*
